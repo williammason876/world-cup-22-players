bbea9193072ee825a3ca9d350bc55920a899d2fc
CLMason <clrumbold@gmail.com>
2025-06-04 11:25:32 -0400

    minor update to build

--- data_collection.py
+++ data_collection.py
@@ -1,8 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
 
+def get_valid_year():
+    while True:
+        try:
+            year = int(input("Enter a World Cup year (1930-2022, every 4 years, excluding 1942 and 1946): "))
+            if year >= 1930 and year <= 2022 and (year - 1930) % 4 == 0 and year not in [1942, 1946]:
+                return year
+            else:
+                print("Invalid year. Please enter a valid World Cup year.")
+        except ValueError:
+            print("Invalid input. Please enter a numeric year.")
+
+
+# Prompt user for the World Cup year
+wc_year = get_valid_year()
+
 BASE_URL = 'https://en.wikipedia.org/'
-wc_url = 'wiki/2022_FIFA_World_Cup_squads'
+wc_url = f'wiki/{wc_year}_FIFA_World_Cup_squads'
 squads = requests.get(BASE_URL + wc_url)
 soup = BeautifulSoup(squads.content, 'html.parser')
 player_class = 'nat-fs-player'
@@ -13,13 +28,16 @@ players = []
 # number, position, Name, DOB String, caps, goals, Club
 for res in results:
     links = res.find_all('a')
-
     player_table_string = res.text.split('\n')
+    print(len(player_table_string))
+    print("\n")
+    print(player_table_string)
     player = {}
     player['position'] = player_table_string[3][1:3]
     player['name'] = player_table_string[5]
     player['dob_string'] = player_table_string[7]
-    player['club'] = player_table_string[13].strip()
+    # usually second to last item
+    player['club'] = player_table_string[len(player_table_string) -2].strip()
     print(player)
 
     player_page = links[1]['href']
@@ -28,10 +46,13 @@ for res in results:
     player_page_visit = requests.get(BASE_URL + player_page)
     player_page_parsed = BeautifulSoup(player_page_visit.content, 'html.parser')
     birth_place = player_page_parsed.find('td', class_='birthplace')
-    print(birth_place.text)
+    if birth_place is not None:
+        print("raw birth place " + str(birth_place))
 
-    # parse and find player's birthplace
-    player['birthplace'] = birth_place.text.replace('\n', '')
+        # parse and find player's birthplace, may need to handle null values
+        player['birthplace'] = birth_place.text.replace('\n', '')
+    else:
+        player['birthplace'] = None
     # parse links and find country player plays for
     helpful_link_items = player_page_parsed.findAll('td', class_='infobox-data-a')
     player['country'] = helpful_link_items[len(helpful_link_items) -1 ].text.replace('\n', '')
@@ -39,5 +60,6 @@ for res in results:
     players.append(player)
 
 import json
-with open('players.json', 'w') as f:
+
+with open(f'{wc_year}_players.json', 'w') as f:
     json.dump(players, f)
